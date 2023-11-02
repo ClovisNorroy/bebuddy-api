5063f395f9cb37f04c838ab19c2467fe6f309bbf
ClovisNorroy <clovis.norroy@gmail.com>
2023-11-02 16:54:01 +0100

    Add User Profile

---
+++ src/Entity/UserProfile.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserProfileRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: UserProfileRepository::class)]
+class UserProfile
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::SMALLINT, nullable: true)]
+    private ?int $age = null;
+
+    #[ORM\OneToOne(inversedBy: 'userProfile', cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $fk_user = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(?int $age): static
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getFkUser(): ?User
+    {
+        return $this->fk_user;
+    }
+
+    public function setFkUser(User $fk_user): static
+    {
+        $this->fk_user = $fk_user;
+
+        return $this;
+    }
+}
