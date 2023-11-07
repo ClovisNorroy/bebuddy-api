cfcd7af7bd5d6d7df7815a2dbaeddb6f63a16e21
ClovisNorroy <clovis.norroy@gmail.com>
2023-11-07 23:22:04 +0100

    Change allowed headers

--- public/index.php
+++ public/index.php
@@ -4,9 +4,7 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
-header('Access-Control-Allow-Credentials:true');
-header('Access-Control-Allow-Headers:X-Requested-With, Content-Type, withCredentials');
-
+header("Access-Control-Allow-Credentials: true");
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
